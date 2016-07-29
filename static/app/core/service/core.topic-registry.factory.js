bd6b2bbb0fe837be7c9dbefca53cf666284a79e4
touhid.alam <tua@selise.ch>
2016-07-29 16:59:44 +0600

    add initial implementation for itemSplitter TODO continue recurtion on itemSplitter

--- static/app/core/service/core.topic-registry.factory.js
+++ static/app/core/service/core.topic-registry.factory.js
@@ -6,8 +6,9 @@
         module.factory("TopicRegistry", TopicRegistry);
 
         function TopicRegistry() {
-            var constructor = function(splitter) {
-                this.splitter = splitter || "/";
+            var constructor = function(rootSplitter, itemSplitter) {
+                this.rootSplitter = rootSplitter || "/";
+                this.itemSplitter = itemSplitter || ",";
                 this.registry = {
 
                 };
@@ -23,25 +24,43 @@
             };
 
             constructor.prototype._stepWValue = function(keys, registry, value) {
-                registry = registry || {};
+                registry = _getInitializedHeapIfNotObject(registry);
 
                 if (undefined === keys[0] || "" === keys[0])
                     return value;
 
-                registry[keys[0]] = this._stepWValue(
-                    keys.slice(1),
-                    registry[keys[0]],
-                    value);
+                if (keys[0].indexOf(this.itemSplitter) > -1) {
+                    debugger;
+                    var siblings = keys[0].split(this.itemSplitter);
+                    for (var i = 0; i < siblings.length; i++) {
+                        registry[siblings[i]] = _getInitializedHeapIfNotObject(registry[siblings[i]]);
+                        registry[siblings[i]] = value;
+                    }
+                } else {
+                    registry[keys[0]] = this._stepWValue(
+                        keys.slice(1),
+                        registry[keys[0]],
+                        value);
+                }
 
                 return registry;
+
+                function _getInitializedHeapIfNotObject(heap) {
+                    return (heap === null || typeof heap === "number") && {} || heap || {};
+                }
             };
 
             constructor.prototype.get = function(keyChain) {
-                return this._step(keyChain.split(this.splitter), this.registry);
+                return this._step(
+                    keyChain.split(this.rootSplitter),
+                    this.registry);
             };
 
             constructor.prototype.set = function(keyChain, value) {
-                return this._stepWValue(keyChain.split(this.splitter), this.registry, value);
+                return this._stepWValue(
+                    keyChain.split(this.rootSplitter),
+                    this.registry,
+                    value);
             }
 
             return this.registry;
