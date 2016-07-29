ac4c8702c9f9a7620d47f763c1909d8f59de1810
touhid.alam <tua@selise.ch>
2016-07-29 16:27:12 +0600

    add initial implementation for _stepWValue

--- static/app/core/service/core.topic-registry.factory.js
+++ static/app/core/service/core.topic-registry.factory.js
@@ -6,57 +6,42 @@
         module.factory("TopicRegistry", TopicRegistry);
 
         function TopicRegistry() {
-            var constructor = function() {
-                this.splitter = "/";
+            var constructor = function(splitter) {
+                this.splitter = splitter || "/";
                 this.registry = {
 
                 };
             };
 
             constructor.prototype._step = function(keys, registry) {
-                if (undefined !== keys[0] && "" !== keys[0])
-                    return this._step(
-                        keys.slice(1),
-                        registry[keys[0]]);
-                return registry;
+                if (undefined === keys[0] || "" === keys[0])
+                    return registry;
+
+                return this._step(
+                    keys.slice(1),
+                    registry[keys[0]]);
             };
 
-            // constructor.prototype._step = function(keyChain, registry) {
-            //     var keys = keyChain.split(this.splitter);
-            //     if (undefined !== keyChain && "" !== keyChain)
-            //         return this._step(
-            //             keys.slice(1).join(this.splitter),
-            //             registry[keys[0]]);
-            //     return registry;
-            // };
+            constructor.prototype._stepWValue = function(keys, registry, value) {
+                registry = registry || {};
 
-            // constructor.prototype._stepWValue = function(keyChain, registry, value) {
-            //     var keys = keyChain.split(this.splitter);
-            //     if (undefined !== keyChain && "" !== keyChain)
-            //         return this._step(
-            //             keys.slice(1).join(this.splitter),
-            //             registry[keys[0]],
-            //             value);
-            //     return registry;
-            // };
+                if (undefined === keys[0] || "" === keys[0])
+                    return value;
 
-            // var a = new constructor();
-            // a.registry = {
-            //     a: {
-            //         b: {
-            //             c: 1
-            //         }
-            //     }
-            // };
+                registry[keys[0]] = this._stepWValue(
+                    keys.slice(1),
+                    registry[keys[0]],
+                    value);
 
-            // a._step("a/b");
+                return registry;
+            };
 
             constructor.prototype.get = function(keyChain) {
                 return this._step(keyChain.split(this.splitter), this.registry);
             };
 
             constructor.prototype.set = function(keyChain, value) {
-
+                return this._stepWValue(keyChain.split(this.splitter), this.registry, value);
             }
 
             return this.registry;
